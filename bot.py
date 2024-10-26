42ff12c5931f2a73b96da486ffff2c1edc842f11
bennettbDEV <bennettbeltran1@gmail.com>
2024-10-26 18:36:03 -0500

    Refactoring script into a class and adding functionality

--- bot.py
+++ bot.py
@@ -6,110 +6,156 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 import asyncio
 
-# Load environment variables
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
 
-# Variables
-selected_channel = None
-c_prefix = "$"
+class ReminderBot(commands.Bot):
+    SETTINGS_FILE = "settings.json"
 
-# Set up the bot
-intents = discord.Intents.default()
-intents.messages = True
-intents.message_content = True
-intents.guilds = True
-bot = commands.Bot(command_prefix=c_prefix, intents=intents)
+    def __init__(self, command_prefix, intents):
+        super().__init__(command_prefix=command_prefix, intents=intents)
+        self.c_prefix = command_prefix
+        self.military_time = False
+        self.load_settings()
 
-SETTINGS_FILE = "settings.json"
+    def load_settings(self):
+        try:
+            with open(self.SETTINGS_FILE, "r") as file:
+                self.settings = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            self.settings = {
+                "channel_name": None,
+                "hour": 14,
+                "minute": 0,
+            }  # Default to 2:00 PM
 
-def load_settings():
-    try:
-        with open(SETTINGS_FILE, "r") as f:
-            return json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return {"channel_name": None, "hour": 14, "minute": 0}  # Default to 2:00 PM
+    def save_settings(self, channel_name=None, hour=None, minute=None):
+        if channel_name is not None:
+            self.settings["channel_name"] = channel_name
+        if hour is not None and minute is not None:
+            self.settings["hour"] = hour
+            self.settings["minute"] = minute
+        with open(self.SETTINGS_FILE, "w") as file:
+            json.dump(self.settings, file)
 
-def save_settings(channel_name=None, hour=None, minute=None):
-    settings = load_settings()
-    if channel_name is not None:
-        settings["channel_name"] = channel_name
-    if hour is not None and minute is not None:
-        settings["hour"] = hour
-        settings["minute"] = minute
-    with open(SETTINGS_FILE, "w") as f:
-        json.dump(settings, f)
+    def enable_24H(self):
+        self.military_time = True
 
+    def disable_24H(self):
+        self.military_time = False
 
-def time_until_reminder(hour, minute):
-    now = datetime.now()
-    target_time = now.replace(hour=hour, minute=minute, second=0, microsecond=0)
-    if now > target_time:
-        target_time += timedelta(days=1)
-    return (target_time - now).total_seconds()
+    async def get_time_until_reminder(self):
+        current_time = datetime.now()
+        target_time = current_time.replace(
+            hour=self.settings["hour"],
+            minute=self.settings["minute"],
+            second=0,
+            microsecond=0,
+        )
+        if current_time > target_time:
+            target_time += timedelta(days=1)
+        return (target_time - current_time).total_seconds()
 
+    @tasks.loop(hours=24)
+    async def send_daily_message(self):
+        await self.wait_until_ready()
+        await asyncio.sleep(await self.get_time_until_reminder())
 
-@tasks.loop(hours=24)
-async def send_daily_message():
-    await bot.wait_until_ready()
+        cur_time = datetime.now()
+        date = cur_time.strftime("%m/%d")
 
-    settings = load_settings()
-    hour, minute = settings["hour"], settings["minute"]
-    await asyncio.sleep(await time_until_reminder(hour, minute))
+        # Skip weekends
+        if cur_time.weekday() >= 5:
+            return
 
-    channel_name = settings.get("channel_name")
-    if channel_name is None:
-        print("Error: No channel has been set. Use the `$setchannel` command to set one.")
-        return
-
-    cur_time = datetime.now()
-    date = cur_time.strftime("%m/%d/%Y")
-
-    # Skip weekends
-    day_of_week = cur_time.strftime("%w")
-    if day_of_week not in range(1,6):
-        return
-    
-    # Search for the channel by name
-    channel_found = False
-    for guild in bot.guilds:
-        channel = discord.utils.get(guild.text_channels, name=channel_name)
-        if channel:
-            channel_found = True
-            message = await channel.send(
-                "@everyone remember to post your standup at 2pm!"
+        channel_name = self.settings.get("channel_name")
+        if channel_name is None:
+            print(
+                f"Error: No channel has been set. Use the `{self.c_prefix}setchannel` command to set one."
             )
-            await message.create_thread(
-                name=f"{date} Standup", auto_archive_duration=60
-            )
-            break
-    if not channel_found:
+            return
+
+        # Search for the channel
+        for guild in self.guilds:
+            channel = discord.utils.get(guild.text_channels, name=channel_name)
+            if channel:
+                h, m = self.settings.get("hour"), self.settings.get("minute")
+                if self.military_time:
+                    time_msg = f"{h}:{m}"
+                else:
+                    if h > 12:
+                        time_msg = f"{int(h)-12}:{m}pm"
+                    else:
+                        time_msg = f"{h}:{m}am"
+                message = await channel.send(
+                    f"@everyone remember to post your standup at {time_msg}!"
+                )
+                await message.create_thread(
+                    name=f"{date} Standup", auto_archive_duration=60
+                )
+                return
         print(f"Error: No channel named '{channel_name}' found.")
 
+    async def on_ready(self):
+        print(f"Bot connected as {self.user}")
+        if not self.send_daily_message.is_running():
+            self.send_daily_message.start()
 
-@bot.command(name="setchannel")
-async def set_channel(ctx, *, channel_name: str):
-    save_settings(channel_name=channel_name)
-    await ctx.send(f"Reminder channel set to #{channel_name}")
-
-@bot.command(name="settime")
-async def set_time(ctx, time_str: str):
-    try:
-        hour, minute = map(int, time_str.split(":"))
-        if not (0 <= hour < 24 and 0 <= minute < 60):
-            raise ValueError
-        save_settings(hour=hour, minute=minute)
-        await ctx.send(f"Reminder time set to {hour:02}:{minute:02}.")
-    except ValueError:
-        await ctx.send("Please provide a valid time in HH:MM format (24-hour).")
+    async def is_moderator_check(self, ctx):
+        return (
+            ctx.author.guild_permissions.manage_messages
+            or ctx.author.guild_permissions.ban_members
+        )
 
 
-@bot.event
-async def on_ready():
-    print(f"Bot connected as {bot.user}")
-    if not send_daily_message.is_running():
-        send_daily_message.start()
+def main():
+    # Load environment variables
+    load_dotenv()
+    TOKEN = os.getenv("DISCORD_TOKEN")
+
+    # Commands
+    @commands.command(name="setchannel")
+    async def set_channel(ctx, *, channel_name: str):
+        ctx.bot.save_settings(channel_name=channel_name)
+        await ctx.send(f"Reminder channel set to #{channel_name}")
+
+    @commands.command(name="settime")
+    async def set_time(ctx, time_str: str):
+        try:
+            hour, minute = map(int, time_str.split(":"))
+            if not (0 <= hour < 24 and 0 <= minute < 60):
+                raise ValueError
+            ctx.bot.save_settings(hour=hour, minute=minute)
+            await ctx.send(f"Reminder time set to {hour:02}:{minute:02}.")
+        except ValueError:
+            await ctx.send("Please provide a valid time in HH:MM format (24-hour).")
+
+    @commands.command(name="enable24H")
+    async def enable_24H_format(ctx):
+        ctx.bot.enable_24H()
+        await ctx.send("24H time format enabled")
+
+    @commands.command(name="disable24H")
+    async def disable_24H_format(ctx):
+        ctx.bot.disable_24H()
+        await ctx.send("12H time format enabled")
+
+    # Setting up the bot and adding commands
+    intents = discord.Intents.default()
+    intents.messages = True
+    intents.message_content = True
+    intents.guilds = True
+    bot = ReminderBot(command_prefix="$", intents=intents)
+
+    # Ensure all commands are run by moderator
+    bot.check(bot.is_moderator_check)
+
+    bot.add_command(set_channel)
+    bot.add_command(set_time)
+    bot.add_command(enable_24H_format)
+    bot.add_command(disable_24H_format)
+
+    # Start bot
+    bot.run(TOKEN)
 
 
 if __name__ == "__main__":
-    bot.run(TOKEN)
+    main()
